a1ff29d07bef7114d00aa6e630e6c6006f0b1f7d
imletaconnoux <imletaconnoux@gmail.com>
2017-08-21 17:28:04 -0400

    added new/edit forms for users and addresses

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,13 @@ Rails.application.routes.draw do
   resources :orders
   resources :addresses
   resources :users
+
+
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
