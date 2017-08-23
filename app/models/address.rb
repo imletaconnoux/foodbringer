0814fd833de5ca31f2480456e8eebf01b6beb381
imletaconnoux <imletaconnoux@gmail.com>
2017-08-23 15:57:44 -0400

    changed background image

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,8 @@
 class Address < ApplicationRecord
 	belongs_to :user
 
-	def to_s
-		label + "\n" + street_address + "\n" city + ", " + state + " " + zip
-	end
+	# def to_s
+	# 	label + "\n" + street_address + "\n" city + ", " + state + " " + zip
+	# end
 
 end
-
