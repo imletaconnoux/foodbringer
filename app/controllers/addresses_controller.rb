8bb83684a84fcc742ce0ed4853b312bfc2254078
imletaconnoux <imletaconnoux@gmail.com>
2017-08-24 10:44:44 -0400

    added update address feature

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,23 @@
 class AddressesController < ApplicationController
 
   def new
+
     @address = Address.new
   end
+
+  def create
+
+  end
+
+
+  def edit
+    @address = Address.find(params[:id])
+
+  end
+
+  def updated
   
+
+  end
+
 end
